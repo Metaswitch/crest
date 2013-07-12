cc1978eaa1894d2fc83cfe9c1933a76427e04535
Matt Williams <matt.williams@metaswitch.com>
2013-07-12 11:05:05 +0000

    Move from using bootstrap.py to easy_install

--- bootstrap-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-$1/bin/python bootstrap.py
-retval=$?
-if [[ $retval != 0 ]]; then
-  echo -e "\n\nFailed to retrieve some third party Python packages.\nThis is usually caused by http://pypi.python.org being down.\nSee above for full error output.\n"
-fi
-exit $retval
