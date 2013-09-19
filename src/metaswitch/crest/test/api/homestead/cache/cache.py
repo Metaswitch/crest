3a4c4cdfeff3fe73f595ca882dcc4e7fd7c527f6
Alex Hockey <alex.hockey@metaswitch.com>
2013-09-19 09:52:54 +0100

    Testcase tidying

--- src/metaswitch/crest/test/api/homestead/cache/cache.py
+++ src/metaswitch/crest/test/api/homestead/cache/cache.py
@@ -44,15 +44,21 @@ from twisted.python.failure import Failure
 
 from metaswitch.crest.api.homestead.cache.cache import Cache
 
+def MockColumn(name, val):
+    m = mock.MagicMock()
+    m.column.name = name
+    m.column.value = val
+    return m
+
+class Result(object):
+    def __init__(self, deferred):
+        self.callback = mock.MagicMock()
+        deferred.addCallback(self.callback)
+
+    def value(self):
+        return self.callback.call_args[0][0]
+
 class TestCache(unittest.TestCase):
-
-    @staticmethod
-    def MockColumn(name, val):
-        m = mock.MagicMock()
-        m.column.name = name
-        m.column.value = val
-        return m
-
     def setUp(self):
         unittest.TestCase.setUp(self)
 
@@ -66,94 +72,81 @@ class TestCache(unittest.TestCase):
         self.cache = Cache()
 
     def test_put_digest(self):
-        self.cass_client.batch_insert.return_value = defer.Deferred()
-        self.cache.put_digest("priv", "digest")
+        self.cass_client.batch_insert.return_value = batch_insert = defer.Deferred()
+        res = Result(self.cache.put_digest("priv", "digest"))
         self.cass_client.batch_insert.assert_called_once_with(
                                                key="priv",
                                                column_family="impi",
                                                mapping={"digest_ha1": "digest"},
                                                ttl=None)
+        batch_insert.callback(None)
+        self.assertEquals(res.value(), None)
+
 
     def test_put_associated_public_id(self):
-        self.cass_client.batch_insert.return_value = defer.Deferred()
-        self.cache.put_associated_public_id("priv", "kermit")
+        self.cass_client.batch_insert.return_value = batch_insert = defer.Deferred()
+        res = Result(self.cache.put_associated_public_id("priv", "kermit"))
         self.cass_client.batch_insert.assert_called_once_with(
                                              key="priv",
                                              column_family="impi",
                                              mapping={"public_id_kermit": None},
                                              ttl=None)
+        batch_insert.callback(None)
+        self.assertEquals(res.value(), None)
 
     def test_put_ims_subscription(self):
-        self.cass_client.batch_insert.return_value = defer.Deferred()
-        self.cache.put_ims_subscription("pub", "xml")
+        self.cass_client.batch_insert.return_value = batch_insert = defer.Deferred()
+        res = Result(self.cache.put_ims_subscription("pub", "xml"))
         self.cass_client.batch_insert.assert_called_once_with(
                                         key="pub",
                                         column_family="impu",
                                         mapping={"ims_subscription_xml": "xml"},
                                         ttl=None)
+        batch_insert.callback(None)
+        self.assertEquals(res.value(), None)
 
     def test_get_ims_subscription(self):
-        self.cass_client.get_slice.return_value = defer.Deferred()
-
-        get_deferred = self.cache.get_ims_subscription("pub")
-        get_cb = mock.MagicMock()
-        get_deferred.addCallback(get_cb)
+        self.cass_client.get_slice.return_value = get_slice = defer.Deferred()
+        res = Result(self.cache.get_ims_subscription("pub"))
 
         self.cass_client.get_slice.assert_called_once_with(
                                                  key="pub",
                                                  column_family="impu",
                                                  names=["ims_subscription_xml"])
-        self.cass_client.get_slice.return_value.callback(
-                               [self.MockColumn("ims_subscription_xml", "xml")])
-        self.assertEquals(get_cb.call_args[0][0], "xml")
+        get_slice.callback([MockColumn("ims_subscription_xml", "xml")])
+        self.assertEquals(res.value(), "xml")
 
     def test_get_digest_no_pub_id_supp(self):
-        self.cass_client.get_slice.return_value = defer.Deferred()
-
-        get_deferred = self.cache.get_digest("priv")
-        get_cb = mock.MagicMock()
-        get_deferred.addCallback(get_cb)
+        self.cass_client.get_slice.return_value = get_slice = defer.Deferred()
+        res = Result(self.cache.get_digest("priv"))
 
         self.cass_client.get_slice.assert_called_once_with(
                                                  key="priv",
                                                  column_family="impi",
                                                  names=["digest_ha1"])
-        self.cass_client.get_slice.return_value.callback(
-                                      [self.MockColumn("digest_ha1", "digest")])
-        self.assertEquals(get_cb.call_args[0][0], "digest")
+        get_slice.callback([MockColumn("digest_ha1", "digest")])
+        self.assertEquals(res.value(), "digest")
 
     def test_get_digest_no_pub_id_assoc(self):
-        self.cass_client.get_slice.return_value = defer.Deferred()
-
-        get_deferred = self.cache.get_digest("priv", "miss_piggy")
-        get_cb = mock.MagicMock()
-        get_deferred.addCallback(get_cb)
+        self.cass_client.get_slice.return_value = get_slice = defer.Deferred()
+        res = Result(self.cache.get_digest("priv", "miss_piggy"))
 
         self.cass_client.get_slice.assert_called_once_with(
                                    key="priv",
                                    column_family="impi",
                                    names=["digest_ha1", "public_id_miss_piggy"])
-        self.cass_client.get_slice.return_value.callback(
-                                    [self.MockColumn("digest_ha1", "digest"),
-                                     self.MockColumn("public_id_kermit", None)])
-        self.assertEquals(get_cb.call_args[0][0], None)
+        get_slice.callback([MockColumn("digest_ha1", "digest"),
+                            MockColumn("public_id_kermit", None)])
+        self.assertEquals(res.value(), None)
 
     def test_get_digest_right_pub_id(self):
-        self.cass_client.get_slice.return_value = defer.Deferred()
-
-        get_deferred = self.cache.get_digest("priv", "miss_piggy")
-        get_cb = mock.MagicMock()
-        get_deferred.addCallback(get_cb)
+        self.cass_client.get_slice.return_value = get_slice = defer.Deferred()
+        res = Result(self.cache.get_digest("priv", "miss_piggy"))
 
         self.cass_client.get_slice.assert_called_once_with(
                                    key="priv",
                                    column_family="impi",
                                    names=["digest_ha1", "public_id_miss_piggy"])
-        self.cass_client.get_slice.return_value.callback(
-                                [self.MockColumn("digest_ha1", "digest"),
-                                 self.MockColumn("public_id_miss_piggy", None)])
-        self.assertEquals(get_cb.call_args[0][0], "digest")
-
-
-
-
+        get_slice.callback([MockColumn("digest_ha1", "digest"),
+                            MockColumn("public_id_miss_piggy", None)])
+        self.assertEquals(res.value(), "digest")
