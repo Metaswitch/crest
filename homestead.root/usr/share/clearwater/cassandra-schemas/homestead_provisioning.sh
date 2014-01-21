e442eda3c2c778f4e6317ec58b583d84f9a0cafb
Rob Day <Robert.Day@metaswitch.com>
2014-01-21 13:22:51 +0000

    Move back to CQLSH 2 to stop Telephus failures

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]];
 then
-    echo "CREATE KEYSPACE homestead_provisioning WITH REPLICATION =  {'class' : 'SimpleStrategy', 'replication_factor' : 2};
+    echo "CREATE KEYSPACE homestead_provisioning WITH strategy_class='org.apache.cassandra.locator.SimpleStrategy' AND strategy_options:replication_factor=2;
 USE homestead_provisioning;
 CREATE TABLE implicit_registration_sets (id uuid PRIMARY KEY, dummy text) WITH read_repair_chance = 1.0;
 CREATE TABLE service_profiles (id uuid PRIMARY KEY, irs text, initialfiltercriteria text) WITH read_repair_chance = 1.0;
 CREATE TABLE public (public_id text PRIMARY KEY, publicidentity text, service_profile text) WITH read_repair_chance = 1.0;
-CREATE TABLE private (private_id text PRIMARY KEY, digest_ha1 text) WITH read_repair_chance = 1.0;" | cqlsh -3
+CREATE TABLE private (private_id text PRIMARY KEY, digest_ha1 text) WITH read_repair_chance = 1.0;" | cqlsh -2
 fi
