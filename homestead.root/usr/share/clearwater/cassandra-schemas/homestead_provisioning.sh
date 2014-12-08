189d6976bda69b4f502051ef81c18161bf1c38d9
Andy Caldwell <andrew.caldwell@metaswitch.com>
2014-12-08 15:10:18 +0000

    Fix up root check and move homestead-prov schema creation to the signaling namespace

--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -15,28 +15,28 @@ header="Waiting for Cassandra"
 let "cnt=0"
 $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
 while [ $? -ne 0 ]; do
-    sleep 1
-    printf "${header}."
-    header=""
-    let "cnt=$cnt + 1"
-    if [ $cnt -gt 120 ]; then
-	printf "*** ERROR: Cassandra did not come online!\n"
-	exit 1
-    fi
-    $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
-    done
+  sleep 1
+  printf "${header}."
+  header=""
+  let "cnt=$cnt + 1"
+  if [ $cnt -gt 120 ]; then
+    printf "*** ERROR: Cassandra did not come online!\n"
+    exit 1
+  fi
+  $namespace_prefix netstat -na | grep -q ":7199[^0-9]"
+done
 let "cnt=0"
 $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
 while [ $? -ne 0 ]; do
-    sleep 1
-    printf "${header}+"
-    header=""
-    let "cnt=$cnt + 1"
-    if [ $cnt -gt 120 ]; then
-	printf "*** ERROR: Cassandra did not come online!\n"
-	exit 1
-    fi
-    $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
+  sleep 1
+  printf "${header}+"
+  header=""
+  let "cnt=$cnt + 1"
+  if [ $cnt -gt 120 ]; then
+    printf "*** ERROR: Cassandra did not come online!\n"
+    exit 1
+  fi
+  $namespace_prefix netstat -na | grep "LISTEN" | awk '{ print $4 }' | grep -q ":9160\$"
 done
 
 if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]];
