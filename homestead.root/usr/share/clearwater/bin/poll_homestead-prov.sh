458fdf5fb85acca1f8cdc9680ecf2a5aa2563c46
Eleanor Merry <eleanor.merry@metaswitch.com>
2015-03-10 17:23:06 +0000

    Monit changes

--- homestead.root/usr/share/clearwater/bin/poll_homestead-prov.sh
+++ homestead.root/usr/share/clearwater/bin/poll_homestead-prov.sh
@@ -34,22 +34,6 @@
 # under which the OpenSSL Project distributes the OpenSSL toolkit software,
 # as those licenses appear in the file LICENSE-OPENSSL.
 
-# This script uses HTTP to poll a homestead-prov process and check whether it is healthy.
-
-# In case homestead-prov has only just restarted, give it a few seconds to come up.
-sleep 5
-
-# Send HTTP request and check that the response is "OK".
-http_url=http://127.0.0.1:8889/ping
-curl -f -g -m 2 -s $http_url 2> /tmp/poll-homestead_prov.sh.stderr.$$ | tee /tmp/poll-homestead_prov.sh.stdout.$$ | head -1 | egrep -q "^OK$"
-rc=$?
-
-# Check the return code and log if appropriate.
-if [ $rc != 0 ] ; then
-  echo HTTP failed to $http_url             >&2
-  cat /tmp/poll-homestead_prov.sh.stderr.$$ >&2
-  cat /tmp/poll-homestead_prov.sh.stdout.$$ >&2
-fi
-rm -f /tmp/poll-homestead_prov.sh.stderr.$$ /tmp/poll-homestead_prov.sh.stdout.$$
-
-exit $rc
+. /etc/clearwater/config
+/usr/share/clearwater/bin/poll-http $local_ip:8889
+exit $?
