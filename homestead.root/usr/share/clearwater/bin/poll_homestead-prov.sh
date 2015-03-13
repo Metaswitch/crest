0679cd6e021c335ec927ae2fe758a666a13636db
Eleanor Merry <eleanor.merry@metaswitch.com>
2015-03-13 13:55:03 +0000

    Change order and remove CPU

--- homestead.root/usr/share/clearwater/bin/poll_homestead-prov.sh
+++ homestead.root/usr/share/clearwater/bin/poll_homestead-prov.sh
@@ -34,7 +34,18 @@
 # under which the OpenSSL Project distributes the OpenSSL toolkit software,
 # as those licenses appear in the file LICENSE-OPENSSL.
 
-. /etc/clearwater/config
-http_ip=$(/usr/share/clearwater/bin/bracket_ipv6_address.py $local_ip)
-/usr/share/clearwater/bin/poll-http $http_ip:8889
-exit $?
+
+# Send HTTP request and check that the response is "OK".
+http_url=http://127.0.0.1:8889/ping
+curl -f -g -m 2 -s $http_url 2> /tmp/poll-homestead_prov.sh.stderr.$$ | tee /tmp/poll-homestead_prov.sh.stdout.$$ | head -1 | egrep -q "^OK$"
+rc=$?
+
+# Check the return code and log if appropriate.
+if [ $rc != 0 ] ; then
+  echo HTTP failed to $http_url             >&2
+  cat /tmp/poll-homestead_prov.sh.stderr.$$ >&2
+  cat /tmp/poll-homestead_prov.sh.stdout.$$ >&2
+fi
+rm -f /tmp/poll-homestead_prov.sh.stderr.$$ /tmp/poll-homestead_prov.sh.stdout.$$
+
+exit $rc
