ea6585d4788a475b99c6636c38f457a9ad631889
Eleanor Merry <eleanor.merry@metaswitch.com>
2015-05-18 11:33:37 +0000

    Add homestead-prov plugin

---
+++ homestead.root/usr/share/clearwater/clearwater-cluster-manager/plugins/homestead_prov_cassandra_plugin.py
@@ -0,0 +1,78 @@
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2015  Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
+
+from metaswitch.clearwater.cluster_manager.plugin_base import \
+    SynchroniserPluginBase
+from metaswitch.clearwater.cluster_manager.plugin_utils import \
+    run_command
+import logging
+
+_log = logging.getLogger("homestead_prov_cassandra_plugin")
+
+
+class HomesteadProvCassandraPlugin(SynchroniserPluginBase):
+    def __init__(self, _ip, local_site, remote_site):
+        self._key = "/clearwater/{}/homestead_prov/clustering/cassandra".format(local_site)
+        self._ip = ip
+
+    def key(self):
+        return self._key
+
+    def should_be_in_cluster(self):
+        return False
+
+    def files(self):
+        pass
+
+    def on_cluster_changing(self, cluster_view):
+        pass
+
+    def on_joining_cluster(self, cluster_view):
+        # The Homestead-prov clustering is done by the Homestead plugin. 
+        # This plugin is only used to insert the Homestead-prov schema
+        if (self._ip == cluster_view.keys().sort()[0]):
+            _log.debug("Adding Homestead-prov schema")
+            run_command(/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh)
+
+    def on_new_cluster_config_ready(self, cluster_view):
+        pass
+
+    def on_stable_cluster(self, cluster_view):
+        pass
+
+    def on_leaving_cluster(self, cluster_view):
+        pass
+
+
+def load_as_plugin(ip, local_site, remote_site):
+    return HomesteadProvCassandraPlugin(ip, local_site, remote_site)
