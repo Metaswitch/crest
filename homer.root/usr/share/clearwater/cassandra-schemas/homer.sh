697b7fd76c3379c9f1ceb66d62b2ef8e496a7a58
Graeme Robertson <graeme.robertson@metaswitch.com>
2015-12-15 16:57:26 +0000

    Create Homer GR schema

--- homer.root/usr/share/clearwater/cassandra-schemas/homer.sh
+++ homer.root/usr/share/clearwater/cassandra-schemas/homer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. /etc/clearwater/config
 
 if [[ ! -e /var/lib/cassandra/data/homer ]];
 then
@@ -16,7 +17,24 @@ then
     /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
   done
 
-  echo "CREATE KEYSPACE homer WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 2};
+  replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
+
+  # If local_site_name and remote_site_names are set then this is a GR
+  # deployment. Set the replication strategy to NetworkTopologyStrategy and
+  # define the sites.
+  if [ -n $local_site_name ] && [ -n $remote_site_names ]
+  then
+    IFS=',' read -a remote_site_names_array <<< "$remote_site_names"
+    replication_str="{'class': 'NetworkTopologyStrategy', '$local_site_name': 2"
+    for remote_site in "${remote_site_names_array[@]}"
+    do
+      # Set the replication factor for each site to 2.
+      replication_str+=", '$remote_site': 2"
+    done
+    replication_str+="}"
+  fi
+
+  echo "CREATE KEYSPACE homer WITH REPLICATION = $replication_str;
         USE homer;
         CREATE TABLE simservs (user text PRIMARY KEY, value text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | $namespace_prefix cqlsh 
 fi
