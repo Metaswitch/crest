7b541ccfa1ee377a16c05b14897d14a5987d620d
Graeme Robertson <graeme.robertson@metaswitch.com>
2015-12-17 12:49:50 +0000

    Use new replication_string.sh file

--- homer.root/usr/share/clearwater/cassandra-schemas/homer.sh
+++ homer.root/usr/share/clearwater/cassandra-schemas/homer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. /etc/clearwater/config
+. /usr/share/clearwater/cassandra-schemas/replication_string.sh
 
 if [[ ! -e /var/lib/cassandra/data/homer ]];
 then
@@ -19,22 +19,9 @@ then
 
   replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
 
-  # If local_site_name and remote_site_names are set then this is a GR
-  # deployment. Set the replication strategy to NetworkTopologyStrategy and
-  # define the sites.
-  if [ -n $local_site_name ] && [ -n $remote_site_names ]
-  then
-    IFS=',' read -a remote_site_names_array <<< "$remote_site_names"
-    replication_str="{'class': 'NetworkTopologyStrategy', '$local_site_name': 2"
-    for remote_site in "${remote_site_names_array[@]}"
-    do
-      # Set the replication factor for each site to 2.
-      replication_str+=", '$remote_site': 2"
-    done
-    replication_str+="}"
-  fi
-
+  # replication_str is set up by
+  # /usr/share/clearwater/cassandra-schemas/replication_string.sh
   echo "CREATE KEYSPACE homer WITH REPLICATION = $replication_str;
         USE homer;
-        CREATE TABLE simservs (user text PRIMARY KEY, value text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | $namespace_prefix cqlsh 
+        CREATE TABLE simservs (user text PRIMARY KEY, value text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | /usr/share/clearwater/bin/run-in-signaling-namespace cqlsh
 fi
--- homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-. /etc/clearwater/config
+. /usr/share/clearwater/cassandra-schemas/replication_string.sh
 
 if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]];
 then
@@ -17,23 +17,8 @@ then
     /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
   done
 
-  replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
-
-  # If local_site_name and remote_site_names are set then this is a GR
-  # deployment. Set the replication strategy to NetworkTopologyStrategy and
-  # define the sites.
-  if [ -n $local_site_name ] && [ -n $remote_site_names ]
-  then
-    IFS=',' read -a remote_site_names_array <<< "$remote_site_names"
-    replication_str="{'class': 'NetworkTopologyStrategy', '$local_site_name': 2"
-    for remote_site in "${remote_site_names_array[@]}"
-    do
-      # Set the replication factor for each site to 2.
-      replication_str+=", '$remote_site': 2"
-    done
-    replication_str+="}"
-  fi
-
+  # replication_str is set up by
+  # /usr/share/clearwater/cassandra-schemas/replication_string.sh
   echo "CREATE KEYSPACE homestead_provisioning WITH REPLICATION = $replication_str;
 USE homestead_provisioning;
 CREATE TABLE implicit_registration_sets (id uuid PRIMARY KEY, dummy text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;
