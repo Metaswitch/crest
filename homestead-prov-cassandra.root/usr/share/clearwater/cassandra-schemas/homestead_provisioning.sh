33e3b1e54189f3b434f3eb9f78b75843f06f1c54
Sebastian Rex <sebastian.rex@metaswitch.com>
2016-12-07 13:00:41 +0000

    Better logs when upgrading
     - log that we're adding/updating cassandra schemas (it may take a while)
     - don't output benign but scary logs indicating cassandra isn't running

--- homestead-prov-cassandra.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead-prov-cassandra.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -8,25 +8,27 @@ cassandra_hostname="127.0.0.1"
 
 quit_if_no_cassandra
 
+echo "Adding/updating Cassandra schemas..."
+
+count=0
+/usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period > /dev/null 2>&1
+
+while [ $? -ne 0 ]; do
+  ((count++))
+  if [ $count -gt 120 ]; then
+    echo "Cassandra isn't responsive, unable to add/update schemas yet"
+    exit 1
+  fi
+
+  sleep 1
+  /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period > /dev/null 2>&1
+done
+
 CQLSH="/usr/share/clearwater/bin/run-in-signaling-namespace cqlsh $cassandra_hostname"
 
 if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]] || \
    [[ $cassandra_hostname != "127.0.0.1" ]];
 then
-  count=0
-  /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
-
-  while [ $? -ne 0 ]; do
-    ((count++))
-    if [ $count -gt 120 ]; then
-      echo "Cassandra isn't responsive, unable to add schemas"
-      exit 1
-    fi
-
-    sleep 1
-    /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
-  done
-
   # replication_str is set up by
   # /usr/share/clearwater/cassandra-schemas/replication_string.sh
   echo "CREATE KEYSPACE homestead_provisioning WITH REPLICATION = $replication_str;
