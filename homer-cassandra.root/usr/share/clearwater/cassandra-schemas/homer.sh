62c6fcecf8c854efbf4f07dcc17cf3bffe289667
Sebastian Rex <sebastian.rex@metaswitch.com>
2016-12-08 09:59:35 +0000

    Better logging when adding homer schemas

--- homer-cassandra.root/usr/share/clearwater/cassandra-schemas/homer.sh
+++ homer-cassandra.root/usr/share/clearwater/cassandra-schemas/homer.sh
@@ -6,31 +6,33 @@ cassandra_hostname="127.0.0.1"
 
 quit_if_no_cassandra
 
+echo "Adding Cassandra schemas..."
+
 CQLSH="/usr/share/clearwater/bin/run-in-signaling-namespace cqlsh $cassandra_hostname"
 
 if [[ ! -e /var/lib/cassandra/data/homer ]] || \
    [[ $cassandra_hostname != "127.0.0.1" ]];
 then
   count=0
-  /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
+  /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period > /dev/null 2>&1
 
   while [ $? -ne 0 ]; do
     ((count++))
     if [ $count -gt 120 ]; then
-      echo "Cassandra isn't responsive, unable to add schemas"
+      echo "Cassandra isn't responsive, unable to add schemas yet"
       exit 1
     fi
 
     sleep 1
-    /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period
+    /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period > /dev/null 2>&1
   done
 
   replication_str="{'class': 'SimpleStrategy', 'replication_factor': 2}"
 
   # replication_str is set up by
   # /usr/share/clearwater/cassandra-schemas/replication_string.sh
-  echo "CREATE KEYSPACE homer WITH REPLICATION = $replication_str;
+  echo "CREATE KEYSPACE IF NOT EXISTS homer WITH REPLICATION = $replication_str;
         USE homer;
-        CREATE TABLE simservs (user text PRIMARY KEY, value text)
+        CREATE TABLE IF NOT EXISTS simservs (user text PRIMARY KEY, value text)
         WITH COMPACT STORAGE AND read_repair_chance = 1.0;" | $CQLSH
 fi
