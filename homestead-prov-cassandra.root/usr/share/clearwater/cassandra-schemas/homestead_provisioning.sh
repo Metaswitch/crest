ea1eca203c6714256cce1a0115a840df0f2db66c
Eleanor Merry <eleanor.merry@metaswitch.com>
2017-08-31 11:24:19 +0000

    Make schemas consistent

--- homestead-prov-cassandra.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
+++ homestead-prov-cassandra.root/usr/share/clearwater/cassandra-schemas/homestead_provisioning.sh
@@ -3,13 +3,14 @@
 cassandra_hostname="127.0.0.1"
 
 . /etc/clearwater/config
-
+. /usr/share/clearwater/utils/check-root-permissions 1
 . /usr/share/clearwater/cassandra_schema_utils.sh
 
 quit_if_no_cassandra
 
 echo "Adding/updating Cassandra schemas..."
 
+# Wait for the cassandra cluster to come online
 count=0
 /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period > /dev/null 2>&1
 
@@ -22,6 +23,7 @@ while [ $? -ne 0 ]; do
 
   sleep 1
   /usr/share/clearwater/bin/poll_cassandra.sh --no-grace-period > /dev/null 2>&1
+
 done
 
 CQLSH="/usr/share/clearwater/bin/run-in-signaling-namespace cqlsh"
@@ -31,8 +33,6 @@ rc=0
 if [[ ! -e /var/lib/cassandra/data/homestead_provisioning ]] || \
    [[ $cassandra_hostname != "127.0.0.1" ]];
 then
-  # replication_str is set up by
-  # /usr/share/clearwater/cassandra-schemas/replication_string.sh
   $CQLSH -e "CREATE KEYSPACE IF NOT EXISTS homestead_provisioning WITH REPLICATION = $replication_str;
              USE homestead_provisioning;
              CREATE TABLE IF NOT EXISTS implicit_registration_sets (id uuid PRIMARY KEY, dummy text) WITH COMPACT STORAGE AND read_repair_chance = 1.0;
